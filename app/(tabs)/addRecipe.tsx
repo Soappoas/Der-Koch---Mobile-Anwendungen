32ed67cadf899e556408e8cda7ddae182fdbf92f
Soappoas <sophia.buettgen@googlemail.com>
2025-04-24 11:21:09 +0200

    added a title input  box with a state

--- app/(tabs)/addRecipe.tsx
+++ app/(tabs)/addRecipe.tsx
@@ -1,19 +1,31 @@
+import React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import {TextInput, ScrollView} from 'react-native';
 
-export default function TabOneScreen() {
+export default function addRecipe() {
+  /* Hier sind die Statese initialisiert, wichtig useState einzeln zu importieren */
+  const [name, setName] = useState('');
+  const [descripton, setDescription] =  useState('');
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    /* nicht einfach view sondern ScrollView damit es scrollbar ist */
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Add a New Recipe</Text>
+      <TextInput
+      style={styles.input}
+      placeholder='Recipe Name'
+      value={name}
+      onChangeText={setName}
+      />
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  /** adjust cotainer todo */
   container: {
     flex: 1,
     alignItems: 'center',
@@ -22,6 +34,13 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 20,
     fontWeight: 'bold',
+  },
+  input: {
+    backgroundColor: 'white',
+    width: '100%',
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
   },
   separator: {
     marginVertical: 30,
